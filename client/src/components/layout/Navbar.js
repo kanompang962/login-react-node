4cdcadd888c75f011d8d8fc0cd11f350318e9e92
tiencahi chamnan <kanompang962@gmail.com>
2023-01-15 22:43:37 +0700

    navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,30 @@
 import React from 'react'
+// design
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+
+
+// Router
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
-    </div>
-  )
-}
+    <Menu mode="horizontal">
+      <Menu.Item key="login" icon={<MailOutlined />}>
+        <Link to="/login">Login</Link>
+      </Menu.Item>
+
+      <Menu.Item key="register" icon={<AppstoreOutlined />}>
+        <Link to="/register">Register</Link>
+      </Menu.Item>
+
+      <Menu.Item key="home" icon={<SettingOutlined />}>
+      <Link to="/home">Home</Link>
+      </Menu.Item>
+
+    </Menu>
+  );
+};
 
 export default Navbar
